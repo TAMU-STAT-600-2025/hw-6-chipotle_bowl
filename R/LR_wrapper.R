bf7c92ac85ff74c2f754c51ddd04ce691c465b72
LeonardoCazares <leonardocazares@tamu.edu>
2025-11-12 17:40:47 -0600

    Apply the Rcpp LRMultiClass_c method to the data.

--- R/LR_wrapper.R
+++ R/LR_wrapper.R
@@ -58,5 +58,10 @@ LRMultiClass <- function(X, y, beta_init = NULL, numIter = 50, eta = 0.1, lambda
       stop("beta_init must be a pxK matrix.")
     beta_init <- beta_init # Initialize beta with the given beta_init
   }
-
+  
+  # Call C++ LRMultiClass_c function to implement the algorithm
+  out = LRMultiClass_c(X, y, beta_init, numIter, eta, lambda)
+  
+  # Return the class assignments
+  return(out)
 }
