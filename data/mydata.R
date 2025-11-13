1b42cda1b5612655fee91b8836d83292ff8ba244
LeonardoCazares <leonardocazares@tamu.edu>
2025-11-12 19:43:21 -0600

    Add the data description.

---
+++ data/mydata.R
@@ -0,0 +1,20 @@
+#' Example dataset: mydata
+#'
+#' A short description of the dataset. This should be 1–2 sentences explaining
+#' what the dataset represents and how it is used.
+#'
+#' @format A data frame from a three normal distributions
+#'
+#' @details
+#' The dataset was generated using Python
+#'
+#' @usage data(mydata)
+#'
+#' @examples
+#' data(mydata)
+#' head(mydata)
+#'
+#' @docType data
+#' @keywords datasets
+#' @name mydata
+NULL
